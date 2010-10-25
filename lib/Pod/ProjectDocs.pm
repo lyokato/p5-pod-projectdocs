7a66e279d126066437ece6716a57bbc2e1334231
lyokato <lyo.kato@gmail.com>
2010-10-26 03:08:18 +0900

    updated version number ,Changes file, and MANIFEST.SKIP

--- lib/Pod/ProjectDocs.pm
+++ lib/Pod/ProjectDocs.pm
@@ -16,7 +16,7 @@ use Pod::ProjectDocs::IndexPage;
 
 __PACKAGE__->mk_accessors(qw/managers components config/);
 
-our $VERSION = '0.38';
+our $VERSION = '0.39';
 
 sub new {
     my $class = shift;
